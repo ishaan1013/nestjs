7a001d3c2593ede1f3a33c4993dc236cc9312cbe
Ishaan Dey <ishaan1013@gmail.com>
2024-01-19 14:31:37 -0500

    Implement logging

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get('LoggingService'));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3000);
